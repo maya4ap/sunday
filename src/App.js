770eecdf731e9243f814f9cdbe7fc2b03a2bc999
Maya Apotheker <maya4ap@gmail.com>
2020-03-27 21:38:38 -0700

    redirect prog

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	
+	  return (
+	  
+		<Router>
+		<div className="App">
+
+		<Route path="/" render={
+		()=>{
+			return (<h1>Welcome home</h1>)
+		}
+	}/>
+
+		</div>
+		</Router>
+		
+		
+	  );
 }
 
 export default App;
