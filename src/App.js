d28a991996d028acb25a2a8eb52ae71b8dd45e85
Maya Apotheker <maya4ap@gmail.com>
2020-04-05 18:27:22 -0700

    trying google charts   calender up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+
+import { Chart } from "react-google-charts";
+
 import './App.css';
 import Form from './Form';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -7,8 +10,58 @@ import Particles from 'react-particles-js';
 
 /*
 * Sunday
-*
+* comment
 */
+
+const options = {
+  title: "Age vs. Weight comparison",
+  hAxis: { title: "Age", viewWindow: { min: 0, max: 15 } },
+  vAxis: { title: "Weight", viewWindow: { min: 0, max: 15 } },
+  legend: "none"
+};
+const data = [
+  ["Age", "Weight"],
+  [8, 12],
+  [4, 5.5],
+  [11, 14],
+  [4, 5],
+  [3, 3.5],
+  [6.5, 7]
+];
+const ExampleChart = () => {
+  return (
+  
+  
+ <Chart
+  width={1000}
+  height={350}
+  chartType="Calendar"
+  loader={<div>Loading Chart</div>}
+  data={[
+    [{ type: 'date', id: 'Date' }, { type: 'number', id: 'Won/Loss' }],
+    [new Date(2012, 3, 13), 37032],
+    [new Date(2012, 3, 14), 38024],
+    [new Date(2012, 3, 15), 38024],
+    [new Date(2012, 3, 16), 38108],
+    [new Date(2012, 3, 17), 38229],
+    [new Date(2013, 1, 4), 38177],
+    [new Date(2013, 1, 5), 38705],
+    [new Date(2013, 1, 12), 38210],
+    [new Date(2013, 1, 13), 38029],
+    [new Date(2013, 1, 19), 38823],
+    [new Date(2013, 1, 23), 38345],
+    [new Date(2013, 1, 24), 38436],
+    [new Date(2013, 2, 10), 38447],
+  ]}
+  options={{
+    title: 'Red Sox Attendance',
+  }}
+  rootProps={{ 'data-testid': '1' }}
+/>
+  );
+};
+
+
 const particleOpt ={
             		particles: {
 						number: {
@@ -119,6 +172,94 @@ function App() {
               params={particleOpt}
            
             />
+			</div>
+			)
+		}
+	}/>
+	
+	<Route path="/dashHome" exact render={
+		()=>{
+			return (
+			<div>
+			
+			
+			
+			<Particles 
+              params={particleOpt}
+           
+            />
+			</div>
+			)
+		}
+	}/>
+	
+		<Route path="/dash1" exact render={
+		()=>{
+			return (
+			<div>
+			
+			
+			
+			
+			
+			
+			<ExampleChart/>
+			<Particles 
+              params={particleOpt}
+           
+            />
+			</div>
+			)
+		}
+	}/>
+	
+		<Route path="/dash2" exact render={
+		()=>{
+			return (
+			<div>
+			
+			
+			
+			<Particles 
+              params={particleOpt}
+           
+            />
+			</div>
+			)
+		}
+	}/>
+	
+		<Route path="/dash3" exact render={
+		()=>{
+			return (
+			<div>
+			
+			
+			
+			<Particles 
+              params={particleOpt}
+           
+            />
+			</div>
+			)
+		}
+	}/>
+	
+		<Route path="/dash4" exact render={
+		()=>{
+			return (
+			<div>
+			
+			
+			
+			<Particles 
+              params={particleOpt}
+           
+            />
+			
+			
+			
+			
 			</div>
 			)
 		}
